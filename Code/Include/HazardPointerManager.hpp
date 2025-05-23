37210ef68fa19da689d75ae438213211c3037526
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-05-23 10:56:02 +0200

    [ADD] HazardPointerManager

---
+++ Code/Include/HazardPointerManager.hpp
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2025 Kayou Corporation. All Rights Reserved.
+
+#pragma once
+#include <atomic>
+#include <thread>
+#include <vector>
+#include <unordered_set>
+#include <cassert>
+#include <mutex>
+
+constexpr int MAX_HAZARD_POINTERS = 100;
+
+struct HazardPointer
+{
+    std::atomic<std::thread::id> mId;
+    std::atomic<void*> mPtr;
+};
+
+class HazardPointerManager
+{
+public:
+    static HazardPointerManager& GetInstance();
+
+    HazardPointer* Acquire();
+
+    static void Release(HazardPointer* hp);
+
+    void RetireNode(void* node, void (*deleter)(void*)) const;
+
+    bool IsHazard(void* p) const;
+
+private:
+    HazardPointerManager() = default;
+
+    void Scan(void (*deleter)(void*)) const;
+
+    HazardPointer m_hpRecords[MAX_HAZARD_POINTERS];
+    thread_local static std::vector<void*> m_retired;
+};
---
+++ Code/Source/HazardPointerManager.cpp
@@ -0,0 +1,60 @@
+﻿// Copyright (c) 2025 Kayou Corporation. All Rights Reserved.
+
+#include "HazardPointerManager.hpp"
+
+thread_local std::vector<void*> HazardPointerManager::m_retired;
+
+HazardPointerManager& HazardPointerManager::GetInstance()
+{
+    static HazardPointerManager s_instance;
+    return s_instance;
+}
+
+HazardPointer* HazardPointerManager::Acquire()
+{
+    const std::thread::id this_id = std::this_thread::get_id();
+    for (auto& hpRecord : m_hpRecords)
+    {
+        if (std::thread::id empty; hpRecord.mId.compare_exchange_strong(empty, this_id)) {
+            return &hpRecord;
+        }
+    }
+    throw std::runtime_error("No hazard pointers available");
+}
+
+void HazardPointerManager::Release(HazardPointer* hp)
+{
+    hp->mPtr.store(nullptr);
+    hp->mId.store(std::thread::id());
+}
+
+void HazardPointerManager::RetireNode(void* node, void(* deleter)(void*)) const
+{
+    m_retired.push_back(node);
+    if (m_retired.size() >= 10)  // Scan threshold
+        Scan(deleter);
+}
+
+bool HazardPointerManager::IsHazard(void* p) const
+{
+    for (auto hpRecord : m_hpRecords)
+    {
+        if (hpRecord.mPtr.load() == p)
+            return true;
+    }
+    return false;
+}
+
+void HazardPointerManager::Scan(void(* deleter)(void*)) const
+{
+    std::vector<void*> to_reclaim;
+    for (void* p : m_retired) {
+        if (!IsHazard(p)) {
+            to_reclaim.push_back(p);
+        }
+    }
+    for (void* p : to_reclaim) {
+        deleter(p);
+        std::erase(m_retired, p);
+    }
+}
